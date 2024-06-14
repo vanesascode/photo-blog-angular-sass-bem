2511cfb39007513789c02893e6032cc9aeab1ac4
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-14 18:09:58 +0200

    feat: :sparkles: add the pics and the notfound routes in the app routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { PicslistComponent } from './pics/picslist/picslist.component';
+import { PicdetailsComponent } from './pics/picdetails/picdetails.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PicslistComponent,
+  },
+  {
+    path: 'pics/:id',
+    component: PicdetailsComponent,
+  },
+  // {
+  //   path: 'about',
+  //   component: AboutComponent,
+  // },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
