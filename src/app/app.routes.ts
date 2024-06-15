2bbf342c624c69190c5e9af61bdae8621a80c114
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-15 18:41:59 +0200

    feat: :sparkles: add the about route in the app routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PicslistComponent } from './pics/picslist/picslist.component';
 import { PicdetailsComponent } from './pics/picdetails/picdetails.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   {
@@ -11,10 +12,10 @@ export const routes: Routes = [
     path: 'pics/:id',
     component: PicdetailsComponent,
   },
-  // {
-  //   path: 'about',
-  //   component: AboutComponent,
-  // },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
   {
     path: '**',
     redirectTo: '',
