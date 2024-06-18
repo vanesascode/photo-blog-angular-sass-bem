a6d2b8f5024d4d3a79272a8ad6aea95da6e13e61
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-18 18:21:42 +0200

    feat: :sparkles: add the logic for the pagination in the piclist component file

--- src/app/pics/picslist/picslist.component.ts
+++ src/app/pics/picslist/picslist.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Pic } from '../pic.model';
 import { PicsService } from '../pics.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
@@ -12,15 +12,71 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './picslist.component.scss',
 })
 export class PicslistComponent implements OnInit {
+  constructor(
+    private picsService: PicsService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  isBrowser!: boolean;
   pics: Pic[] = [];
-  constructor(private picsService: PicsService, private router: Router) {}
+  page: number = 1;
+  picsPerPage: number = 8;
+  totalPages!: number;
+
+  setPicsPerPageBasedOnWindowWidth(): void {
+    if (this.isBrowser) {
+      const windowWidth = window.innerWidth;
+      if (windowWidth < 480) {
+        this.picsPerPage = 6;
+      } else if (windowWidth <= 1440) {
+        this.picsPerPage = 12;
+      } else {
+        this.picsPerPage = 15;
+      }
+    }
+  }
 
   ngOnInit(): void {
-    this.picsService.getAllPics().subscribe((data: Pic[]) => {
-      this.pics = data;
+    this.setPicsPerPageBasedOnWindowWidth();
+
+    this.picsService.getAllPics().subscribe((totalPics: Pic[]) => {
+      this.totalPages = Math.ceil(totalPics.length / this.picsPerPage);
     });
+
+    this.picsService
+      .getPicsWithPagination(this.page, this.picsPerPage)
+      .subscribe((data: Pic[]) => {
+        this.pics = data;
+      });
   }
   scrollToTop(): void {
     window.scrollTo(0, 0);
+  }
+
+  nextPage(): void {
+    if (this.page < this.totalPages) {
+      this.picsService
+        .getPicsWithPagination(this.page + 1, this.picsPerPage)
+        .subscribe((data: Pic[]) => {
+          this.pics = data;
+          this.page += 1;
+        });
+    }
+    this.scrollToTop();
+  }
+
+  previousPage(): void {
+    if (this.page > 1) {
+      this.picsService
+        .getPicsWithPagination(this.page - 1, this.picsPerPage)
+        .subscribe((data: Pic[]) => {
+          this.pics = data;
+          this.page -= 1;
+        });
+    }
+    this.scrollToTop();
   }
 }
