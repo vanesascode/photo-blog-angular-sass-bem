4962b925038f2b513598dfaabba040fc7f0189a0
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-26 12:26:08 +0200

    feat: :sparkles: add the logic to go to next image and to previous image in the picdetails component file

--- src/app/pics/picdetails/picdetails.component.ts
+++ src/app/pics/picdetails/picdetails.component.ts
@@ -18,30 +18,58 @@ export class PicdetailsComponent implements OnInit {
   ) {}
   pic!: Pic;
   totalPics!: number;
+  disablePreviousButton!: boolean;
+  disableNextButton!: boolean;
 
   ngOnInit(): void {
+    this.picsService.getAllPics();
+    this.totalPics = this.picsService.totalPics;
+
     this.route.paramMap.subscribe((params: ParamMap) => {
       let id = params.get('id');
       if (id) {
         this.picsService.getPic(parseInt(id, 10)).subscribe((data: any) => {
           this.pic = data;
         });
+        this.disableNextButtonFunction(+id as any as number);
+        this.disablePreviousButtonFunction(+id as any as number);
       }
     });
+  }
 
-    this.picsService.getAllPics();
-    this.totalPics = this.picsService.totalPics;
+  disablePreviousButtonFunction(id: number): void {
+    id = id ?? this.pic.id;
+    if (id === 1) {
+      this.disablePreviousButton = true;
+    } else {
+      this.disablePreviousButton = false;
+    }
+  }
+
+  disableNextButtonFunction(id: number): void {
+    id = id ?? this.pic.id;
+    if (id === this.totalPics) {
+      this.disableNextButton = true;
+    } else {
+      this.disableNextButton = false;
+    }
   }
 
   goBack(): void {
     if (this.pic.id > 1) {
-      this.router.navigateByUrl(`/pics/${this.pic.id - 1}`);
+      const id = this.pic.id - 1;
+      this.router.navigateByUrl(`/pics/${id}`);
+      this.disableNextButtonFunction(id);
+      this.disablePreviousButtonFunction(id);
     }
   }
 
   goNext(): void {
     if (this.pic.id < this.picsService.totalPics) {
-      this.router.navigateByUrl(`/pics/${this.pic.id + 1}`);
+      const id = this.pic.id + 1;
+      this.router.navigateByUrl(`/pics/${id}`);
+      this.disableNextButtonFunction(id);
+      this.disablePreviousButtonFunction(id);
     }
   }
 }
